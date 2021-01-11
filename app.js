7c7c66c50f8924c97dc7cdcc14c801ff0f3b5fd9
Jay Gupta <jaygupt1973@gmail.com>
2021-01-11 14:04:32 -0800

    Add homeStartingContent

--- app.js
+++ app.js
@@ -18,3 +18,7 @@ app.use(express.static("public"));
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+app.get("/", function(req, res){
+  res.render("home", {content: homeStartingContent}); 
+}); 
