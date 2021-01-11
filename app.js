cefcf97e9fa4e3ba0ed04e00cc04cd2e597c1470
Jay Gupta <jaygupt1973@gmail.com>
2021-01-11 12:59:16 -0800

    Add .gitignore

--- app.js
+++ app.js
@@ -15,19 +15,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
